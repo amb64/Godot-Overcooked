3f0f37aee74b30dafd7e90f5657d27d24eff5eb4
JamesEyre21 <124672903+JamesEyre21@users.noreply.github.com>
2023-11-30 12:55:39 +0000

    Visual of how items will combine

---
+++ Entity ontology/Interaction.gd
@@ -0,0 +1,15 @@
+extends RayCast3D
+
+var current_collider
+
+func _process(delta):
+	var collider = get_collider()
+	
+	if is_colliding() and collider is Interactable:
+		if current_collider != collider:
+			current_collider = collider
+		
+		if Input.is_action_just_pressed("Interact"):
+			collider.interact()
+	elif current_collider:
+		current_collider = null
