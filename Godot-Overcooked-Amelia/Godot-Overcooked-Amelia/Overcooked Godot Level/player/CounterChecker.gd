71b8a1bda75c29569c7848e2e3a1740fc1c4ea9d
Water Sandwich <99360875+Water-Sandwich@users.noreply.github.com>
2023-12-14 08:13:04 +0000

    Cleanup and multi item counters
    cleanup for the branch
    added multi item counters(drop and pikcup is good)
    more cleanup necessary for code
    (especially concerning pickup/drop handling on multi item counters, currently just plops it on top of another item)

--- Godot-Overcooked-Amelia/Godot-Overcooked-Amelia/Overcooked Godot Level/player/CounterChecker.gd
+++ Godot-Overcooked-Amelia/Godot-Overcooked-Amelia/Overcooked Godot Level/player/CounterChecker.gd
@@ -6,6 +6,7 @@ var counterItem : Node3D
 var playerItem : Node3D
 var overCounter : bool = false
 var holdingItem : bool = false
+var isMultiCounter : bool
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,15 +17,14 @@ func _ready():
 func _process(delta):
 	var isInteracting : bool = Input.is_action_just_pressed("interact")
 	#pickup
+
 	if(isInteracting && overCounter && !holdingItem && counter_has_item()):
-		counterItem.reparent(playerHolder, false)
-		holdingItem = true
+		pickup()
 		pass
 
 	#drop
-	elif(isInteracting && overCounter && holdingItem && !counter_has_item()):
-		counterItem.reparent(counterHolder, false)
-		holdingItem = false
+	elif(isInteracting && overCounter && holdingItem):
+		drop()
 		pass
 
 
@@ -33,22 +33,38 @@ func _on_body_entered(body:Node3D):
 	if(body.name != "CounterBody"):
 		return
 	
-	print("entered")
 	overCounter = true
-	var counterParent : Node3D = body.get_parent()
+	var counterParent : Node3D = body.get_parent().get_parent()
 	counterHolder = counterParent.get_child(0)
+
+	isMultiCounter = counterHolder.name == "MultiItemHolder"
 
 	if(counterHolder.get_child_count() < 1):
 		return
 	
-	counterItem = counterHolder.get_child(0)
+	var count = counterHolder.get_child_count()
+	counterItem = counterHolder.get_child(count-1) #get last item
+	(counterItem as Node3D).translate(Vector3(0,-count+1,0))
 
 func _on_body_exited(body:Node3D):
 	if(body.name != "CounterBody"):
 		return
 
-	print("exited")
 	overCounter = false
 	
 func counter_has_item() -> bool:
 	return counterHolder.get_child_count() > 0
+
+func pickup():
+	counterItem.reparent(playerHolder, false)
+	holdingItem = true
+
+func drop():
+	if(counter_has_item() && !isMultiCounter):
+		return
+
+	var count = counterHolder.get_child_count()
+	playerHolder.get_child(0).reparent(counterHolder, false) #if .getchild is somehow null then i will not be happy
+	(counterHolder.get_child(count) as Node3D).translate(Vector3(0,count,0))
+
+	holdingItem = false
