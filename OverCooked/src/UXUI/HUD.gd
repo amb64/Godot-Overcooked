de74c089f3aca7b789cf8a7721c0002a5a05d289
TxbyG <26584700@students.lincoln.ac.uk>
2024-01-18 17:25:32 +0000

    Final version

---
+++ OverCooked/src/UXUI/HUD.gd
@@ -0,0 +1,5 @@
+extends Control
+
+# Create a count down animation timer displaying the time left the character has.
+# Add Animation of flashy red when the count down timer is low.
+# When tipped display a reward animation.
