db9d24a3411c6e9c2e6c855180b5af116d638e66
amb64 <123978335+amb64@users.noreply.github.com>
2024-01-25 13:35:28 +0000

    Project to fix

--- src/UXUI/HUD.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-# Create a count down animation timer displaying the time left the character has.
-# Add Animation of flashy red when the count down timer is low.
-# When tipped display a reward animation.
