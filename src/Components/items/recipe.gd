94e74913684442d8011d779f6452816214242c01
D Vilimavicius <99360875+Water-Sandwich@users.noreply.github.com>
2024-03-04 17:51:25 +0000

    more prototyping of ontology
    i am running out of patience

--- src/Components/items/recipe.gd
+++ src/Components/items/recipe.gd
@@ -1,5 +1,19 @@
+@tool
+
 class_name Recipe extends Resource
 
 @export var recipe_name : StringName
-@export var base_ingredients : Array[Item]
-@export var additionals : Array[Item]
+@export var product : BaseItem :
+	set(value):
+		product = value.duplicate()
+	get:
+		return product
+		
+@export var additionals : Array[BaseItem]
+
+#called after an item is modified
+#item is checked against all known recipes
+
+#check to see if it matches ingredient signature
+func item_meets_criteria(item : BaseItem) -> bool:
+	return product == item
--- src/Components/items/resources/base_item.gd
+++ src/Components/items/resources/base_item.gd
@@ -1,3 +1,13 @@
-extends Node
+class_name BaseItem extends Resource
 
-@export var resource : Resource
+const s  = preload("res://src/Components/items/enum.gd")
+
+#ingredients // consists of other items
+@export var item_name : StringName
+@export var state : s.State
+
+func is_equal(item) -> bool:
+	if(item is MixedItem):
+		return false
+		
+	return item_name == item.item_name && state == item.state
