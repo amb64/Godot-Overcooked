037dcab7e02d48b86ed5b889a96ff320e85d5537
Water-Sandwich <deivoro23@gmail.com>
2024-04-11 02:41:50 +0100

    new ontology sorta good
    much happier with how it is now comapared to last time. all recipes are manually set, thus there is no need for any states or that nonesense. overall much simpler
    
    deleted redundant player folder

--- src/Components/counters/primitives/mod.gd
+++ src/Components/counters/primitives/mod.gd
@@ -2,14 +2,18 @@ class_name CounterModifier extends Counter
 
 @export var proccesses : Array[ItemIO]
 
-func action(source : Node3D) -> void:
-	if not source is ItemInstance:
+func action(_source : Node3D) -> void:
+	if item_slots[0].get_child(0) == null:
+		return
+	
+	if not item_slots[0].get_child(0) is ItemInstance:
 		print("nop")
 		return
 	
-	var item : ItemInstance = source
+	var item : ItemInstance = item_slots[0].get_child(0)
 	print("yup")
 	
 	for IO : ItemIO in proccesses:
-		if item.item == IO:
+		if item.item.is_equal(IO.input):
 			print("yipee")
+			item.item = IO.output
