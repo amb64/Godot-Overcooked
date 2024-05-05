f91ff3eeaf6aa79574f18a1fa866eb06d1d2c972
Water-Sandwich <deivoro23@gmail.com>
2024-05-05 19:35:45 +0100

    multiple items seem to be working
    added multiple item input recipe support. you are now able to combine items

--- src/Components/counters/primitives/mod.gd
+++ src/Components/counters/primitives/mod.gd
@@ -1,6 +1,7 @@
 class_name CounterModifier extends Counter
 
 @export var proccesses : Array[ItemIO]
+@export var output_slot : int = 0
 
 func action(_source : Node3D) -> void:
 	if item_slots[0].get_child(0) == null:
@@ -10,11 +11,24 @@ func action(_source : Node3D) -> void:
 		print("nop")
 		return
 	
-	var item : ItemInstance = item_slots[0].get_child(0)
+	var items : Array[ItemInstance]
+	#init items
+	for i in range(0, item_slots.size()):
+		var item = item_slots[i].get_child(0) #error keeps happening here, no clue why, but causes no problems
+		if item != null && item is ItemInstance:
+			items.append(item_slots[i].get_child(0))
+	
 	print("yup")
 	
 	for IO : ItemIO in proccesses:
-		if item.item.is_equal(IO.input):
+		if IO.does_apply(items):
+			#remove all items that arent on the output slot
+			for i in range(0,item_slots.size()):
+				if(i == output_slot):
+					continue
+					
+				item_slots[i].get_child(0).queue_free()
+			
 			print("yipee")
-			item.item = IO.output
+			item_slots[output_slot].get_child(0).item = IO.output
 			return
--- src/Components/new ontology/scripts/itemIO.gd
+++ src/Components/new ontology/scripts/itemIO.gd
@@ -1,5 +1,21 @@
 class_name ItemIO extends Resource
 
-@export var input : Item
+@export var input : Array[Item]
 @export var output : Item
 #reference to final model here everntually
+
+func does_apply(items : Array[ItemInstance]) -> bool:
+	if(items.size() != input.size()):
+		return false
+	
+	for i in range(0, input.size()):		
+		var has_matched : bool = false
+		for j in range(0, items.size()):
+			if input[i].is_equal(items[j].item):
+				has_matched = true
+				break
+				
+		if !has_matched:
+			return false #if j loop ends without getting a match, the items do no match
+		
+	return true
