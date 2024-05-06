3c3afb0b8a263670b47d50bd449a7d8348d0b972
Toby <125763262+TxbyG@users.noreply.github.com>
2024-05-06 19:48:54 +0100

    bla

--- Game.gd
+++ Game.gd
@@ -1,15 +1,15 @@
 extends Node
 
+# The dynamic dictornary that stores level info
 var Levels = {}
 
-var vanPos: Vector3 = Vector3(-14.0, 1.0, 4.0)
+var vanPos: Vector3 = Vector3(-14.8, 0.6, 5.0)
 var vanRot: Vector3 = Vector3(0.0, 0.0, 0.0)
+var MainMenu: PackedScene = preload("res://src/UXUI/MainMenu.tscn") # Spawn MainMenu
+var Tour: PackedScene = preload("res://scenes/WorldTour.tscn") # Spawn World tour
 
-## Drag and drop here. Must be [color=8cf]MainMenu.tscn[/color] file.
-@export var MainMenu: PackedScene # Spawn MainMenu
-## Drag and drop here. Must be [color=8cf]WorldTour.tscn[/color] file.
-@export var Tour: PackedScene # Spawn World tour
-## Drag and drop here. Must be a [color=8cf]Level scene[/color] tscn file.
+## Drag and drop here. 
+## Must be a [color=8cf]Level scene[/color] tscn file.
 ## Must include a packed scene based on int for next level node or it'll crash.
 @export var levels: Array[PackedScene]
 
@@ -19,31 +19,38 @@ func _ready():
 
 # Loads in world tour scene
 func load_world():
+	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
+	for n in get_children(true):
+		if n.name != $WorldEnvironment.name:
+			if n.name != $BackgroundMusic.name:
+				if n.name != $ColourBlindUX.name:
+					n.queue_free()
+	set_postprocess(preload("res://resources/postprocess/WorldTour.tres"))
 	add_child(Tour.instantiate())
-	get_node("MainMenu").queue_free()
 
 # Loads in a level scene
 func load_level(numb: int):
-	var x = levels[numb].instantiate()
-	x.name = "Level" # Helpful for removing after use
-	add_child(x)
-	get_node("WorldTour").queue_free()
-
-# Return back to world tour
-func returnTour():
-	add_child(Tour.instantiate())
-	get_node("Level").queue_free() # delete level scene
+	if Levels.size() > numb:
+		var x = levels[numb].instantiate()
+		x.name = str(numb) # Helpful for IDing the level
+		add_child(x)
+		get_node("WorldTour").queue_free()
 
 # Return to main menu
 func returnMenu():
+	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE)
+	set_postprocess(preload("res://resources/postprocess/MainMenu.tres"))
 	for n in get_children(true):
-		n.queue_free()
+		if n.name != $WorldEnvironment.name:
+			if n.name != $BackgroundMusic.name:
+				if n.name != $ColourBlindUX.name:
+					n.queue_free()
 	add_child(MainMenu.instantiate())
 
 # Level settings
 func loadLevelConfig():
 	var unlocked: bool = true
-	for i in levels.size() - 1:
+	for i in levels.size():
 		if i == 0:
 			unlocked = true
 		else:
@@ -57,10 +64,11 @@ func loadLevelConfig():
 
 # Also used after the game ends to unlock the next level
 func unLockLevelConfig(Level: int):
-	for key in Levels[Level].keys():
-		for point in Levels[Level][key]:
-			if point != int(): # Checks if its a bool value
-				Levels[Level][key][point] = true # set the level to be unlocked
+	if Levels.size() > Level: # prevents overunlocking non levels that dont exist
+		for key in Levels[Level].keys():
+			for point in Levels[Level][key]:
+				if point != int(): # Checks if its a bool value
+					Levels[Level][key][point] = true # set the level to be unlocked
 
 # Used for after the game ends to give how many stars
 func editLevelConfig(Level: int, stars: int):
@@ -68,10 +76,18 @@ func editLevelConfig(Level: int, stars: int):
 		for point in Levels[Level][key]:
 			if point == int(): # Checks if its an int value
 				Levels[Level][key][point] = stars # set star amount
-	unLockLevelConfig(Level + 1)
+				
+	# Must get atleast 1 star inorder to progress
+	if stars > 0:
+		unLockLevelConfig(Level + 1)
 
 
 # postprocess mechanic
-#func get_postprocess(env: Environment):
-	#if env != $WorldEnvironment.environment:
-		#$WorldEnvironment.environment = env
+func set_postprocess(env: Environment):
+	if env != $WorldEnvironment:
+		$WorldEnvironment.environment = env
+
+# background music mechanic
+func set_backgroundMusic(audio: AudioStreamWAV):
+	if audio != $BackgroundMusic:
+		$BackgroundMusic.stream = audio
--- src/UXUI/PauseMenu.gd
+++ src/UXUI/PauseMenu.gd
@@ -4,22 +4,29 @@ var isPaused: bool = false
 
 func _ready():
 	$Menu.visible = false
+	$Background.visible = false
 	$OptionsMenu.visible = false
 
 func _process(_delta):
 	if Input.is_action_just_released("escape"):
 		isPaused = !isPaused
+		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE)
 		if isPaused:
 			isPaused = true
 			$Menu.visible = true
+			$Background.visible = true
+			
 		else:
+			DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
 			isPaused = false
 			$Menu.visible = false
+			$Background.visible = false
 
 # Return back to game
 func _on_return_button_pressed():
 	isPaused = false
 	$Menu.visible = false
+	$Background.visible = false
 
 # Settings
 func _on_options_button_pressed():
